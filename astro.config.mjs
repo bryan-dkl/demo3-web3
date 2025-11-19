4c1ed9798aaea43c2ee7f2e9b0a29516411bb46f
Deketelaere <bryan.deketelaere@student.howest.be>
2025-11-19 11:51:52 +0100

    deploying demo 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://your-github-username.github.io',
-    base: '/cck/',
+    site: 'https://bryan-dkl.github.io',
+    base: '/demo3-web3',
 });
