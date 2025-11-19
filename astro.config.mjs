897cfdbc2e90f779a48ddefe72073c25db477b5a
Deketelaere <bryan.deketelaere@student.howest.be>
2025-11-19 11:53:18 +0100

    changed base in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://bryan-dkl.github.io',
-    base: '/demo3-web3',
+    base: '/demo3-web3/',
 });
